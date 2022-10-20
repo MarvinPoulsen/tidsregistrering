85c62c8d3d6d63cf530c86ab1f0bde288fc2b237
Marvin Poulsen <marpo@lolland.dk>
2022-10-20 16:42:26 +0200

    added piechart and legendtable files

---
+++ internal/src/components/timechart/LegendTable.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { useTable } from 'react-table';
+import './legendTable.scss'
+import colors from '../../colors';
+import { PiechartData } from './PiechartNoLegend'
+
+interface LegendTableProps {
+  headers: string[];
+  data: PiechartData[];
+  onRowToggle: (rowIndex:number) => void;
+}
+
+const createTableData = (data, headers) => {
+  const tableData: object[] = [];
+  const tableColumns: object[] = [];
+  for (let i = 0; i < headers.length; i++){
+    const header = headers[i];
+    tableColumns.push({ 
+        Header: header,
+        title: header, // todo 
+        accessor: 'col'+(i+1).toString(),
+        Footer: i===0 ? 'I alt': data.filter(item=>item.on).reduce((sum, current) => sum + current.value, 0) 
+    })
+  }
+  for (let i = 0; i < data.length; i++) {
+    const tableRow = data[i];
+    tableData.push({ col1: tableRow.name, col2: tableRow.value });
+  }
+
+  return [tableData, tableColumns];
+};
+
+function LegendTable(props: LegendTableProps) {
+  const [data, columns] = React.useMemo(
+    () => createTableData(props.data, props.headers),
+    [props.data, props.headers]
+  );
+  const { getTableProps, getTableBodyProps, headerGroups, footerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+  return (
+    <table
+      {...getTableProps({
+        className:
+          'table is-bordered is-hoverable is-size-7 is-fullwidth is-narrow',
+      })}
+    >
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th {...column.getHeaderProps()}>
+                <abbr title={column.render('title')}>
+                {column.render('Header')}
+                </abbr>
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          const isOff = !props.data[row.index].on          
+          return (
+            <tr {...row.getRowProps()} onClick={()=> props.onRowToggle(row.index)}>
+              {row.cells.map((cell) => {
+                return (
+                  <td {...cell.getCellProps({ className: ' content' +(isOff? ' is-off':'') })}>
+                    {cell.column.id === 'col1' && 
+                      <span 
+                        className='color-box' 
+                        style={{
+                          background: colors.bgColors[row.index],
+                          borderColor: colors.borderColors[row.index]
+                        }}
+                      >                        
+                      </span>
+                    }
+                    {cell.render('Cell')}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+      <tfoot>
+        {footerGroups.map(group => (
+          <tr {...group.getFooterGroupProps()}>
+            {group.headers.map(column => (
+              <th {...column.getFooterProps()}>{column.render('Footer')}</th>
+            ))}
+          </tr>
+        ))}
+      </tfoot>
+    </table>
+  );
+}
+
+export default LegendTable;
---
+++ internal/src/components/timechart/PiechartNoLegend.tsx
@@ -0,0 +1,96 @@
+import React, { useRef, useEffect } from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, InteractionMode } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+import colors from '../../colors';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+export const options = {
+  responsive: true,
+  interaction: {
+    intersect: true,
+    mode: 'dataset' as InteractionMode,
+  },
+  plugins: {
+    legend: {
+      display: false,
+    },
+    tooltip: {
+      filter: (tooltipItem) => {
+        return tooltipItem.chart.getDataVisibility(tooltipItem.dataIndex)
+      },
+      yAlign:'bottom' as 'bottom',
+      titleAlign: 'center' as 'center',
+      callbacks: {
+        label: (context) => {
+          let totalSum = 0;
+          for (let i = 0;i<context.dataset.data.length;i++){				
+            const on = context.chart.getDataVisibility(i);
+            totalSum += (on ? context.dataset.data[i] : 0);
+          };
+          const label = context.label.length > 15 ? context.label.substring(0,11) + '.. ' : context.label;
+          const tooltipContent = `${label}: ${((context.parsed/totalSum)*100).toFixed(1)}%`;
+          return tooltipContent;
+        }, 
+      }
+    },
+    title: {
+      display: false,
+      text: 'demographySognPie',
+    },
+
+  },
+};
+export interface PiechartData {
+  name: string;
+  value: number;
+  on: boolean;
+}
+
+export interface PiechartProps {
+  data: PiechartData[];
+  visibility: boolean[];
+}
+interface PiechartDatasets {
+  data: number[];
+  backgroundColor: string[];
+  borderColor: string[];
+  borderWidth: number;
+}
+interface PieData {
+  labels: string[];
+  datasets: PiechartDatasets[];
+}
+export function PiechartNoLegend(props: PiechartProps) {
+  const chartRef = useRef();
+  useEffect(()=>{
+    for (let i=0; i<props.visibility.length;i++){
+      if (chartRef.current.getDataVisibility(i) !== props.visibility[i]){
+        chartRef.current.toggleDataVisibility(i);
+      }
+    }
+    chartRef.current.update();
+  },[props.visibility])
+  const datasets: PiechartDatasets[] = [
+    {
+      data: props.data.map(row=>row.value),
+      backgroundColor: colors.bgColors,
+      borderColor: colors.borderColors,
+      borderWidth: 1,
+    },
+  ];
+  const data: PieData = {
+    labels: props.data.map(row=>row.name),
+    datasets,
+  };
+  return (
+    <>
+      <Pie
+        ref={chartRef}
+        options={options} 
+        data={data} 
+      />
+    </>
+  );
+}
+
+export default PiechartNoLegend;
