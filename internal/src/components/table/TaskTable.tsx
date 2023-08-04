e67be2772a63e7b323791064ff90a1a0ac7f2673
Marvin Poulsen <marpo@lolland.dk>
2023-08-04 15:35:21 +0200

    edit and delete tasks

--- internal/src/components/table/TaskTable.tsx
+++ internal/src/components/table/TaskTable.tsx
@@ -1,49 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTable } from 'react-table';
 import Icon from '@mdi/react';
-import { mdiPencil, mdiDelete, mdiNumeric1Circle, mdiNumeric2CircleOutline, mdiNumeric3 } from '@mdi/js';
+import { mdiPencil, mdiDelete, mdiDeleteOff, mdiNumeric1Circle, mdiNumeric2CircleOutline, mdiNumeric3, mdiNull } from '@mdi/js';
 import { Task, Project } from '../../SPS';
 import format from 'date-fns/format';
 // import {toHoursAndMinutes} from '../../utils'
 
 interface TaskTableProps {
     tasks: Task[];
-    projects: Project[]
+    projects: Project[];
+    projectId: number;
+    setProjectId: (projectId) => void;
+    taskName: string;
+    setTaskName: (taskName) => void;
+    milestone: Date;
+    setMilestone: (milestone) => void;
+    importance: number;
+    setImportance: (importance) => void;
+    timeframe: number;
+    setTimeframe: (timeframe) => void;
+    description: string;
+    setDescription: (description) => void;
+    obsolete: boolean;
+    setObsolete: (isObsolete: boolean) => void;
+    onSave: () => void;
+    setEditEntry: (id) => void;
+    resetForm: () => void;
+    setIsNewTaskActive: (isOn: boolean) => void;
 }
-function TaskTable(props: TaskTableProps) {    
+function TaskTable(props: TaskTableProps) {
     // console.log('TaskTableProps: ',props)
-    const handleOnEdit = (element)=>{
-        console.log('element: ',element)
-    }
-    const onDelete = (e)=>{
-        console.log('e: ',e)
-    }
-    const tasks = props.tasks.map((element) => {
-        const milestone = format(element.milestone, "dd-MM-yyyy")
-        const importance = element.importance < 1 ? '' :
-            // <Icon path={mdiNull} size={1} color="blue"/> :
-            element.importance < 2 ?
-                <Icon path={mdiNumeric1Circle} size={1} color="red"/> :
-                element.importance < 3 ?
-                    <Icon path={mdiNumeric2CircleOutline} size={1} color="orange"/> :
-                    <Icon path={mdiNumeric3} size={1} color="green"/> ;
+    const[isObsoleteId,setIsObsoleteId] = useState<number>(null);
 
-    const project = props.projects.find((p) => p.id === element.projectId);
+    
+    const closeModal = () => {
+        props.resetForm();
+        setIsObsoleteId(null);
+    };
+    
+    const confirmObsolete = () => {
+        // console.log('isObsoleteId: ',isObsoleteId)
+        // console.log('props.projectId: ',props.projectId)
+        // console.log('props.taskName: ',props.taskName)
+        // console.log('props.milestone: ',props.milestone)
+        // console.log('props.importance: ',props.importance)
+        // console.log('props.timeframe: ',props.timeframe)
+        // console.log('props.description: ',props.description)
+        // console.log('props.obsolete: ',props.obsolete)
+        // console.log('props: ',props)
+        props.onSave()
+        closeModal();
+    };
+    const handleOnEdit = (element) => {
+        console.log('obsolete: ',props.obsolete)
+        console.log('element: ', element);
+        props.setEditEntry(element.id);
+        props.setProjectId(element.projectId);
+        props.setTaskName(element.taskName);
+        props.setMilestone(new Date(element.milestone));
+        props.setImportance(element.importance);
+        props.setTimeframe(element.timeframe);
+        props.setDescription(element.description);
+        props.setObsolete(element.obsolete);
+        props.setIsNewTaskActive(true)
+    };
+    const onObsolete = (element) => {
+        console.log('element: ', element);
+        props.setEditEntry(element.id);
+        props.setProjectId(element.projectId);
+        props.setTaskName(element.taskName);
+        props.setMilestone(new Date(element.milestone));
+        props.setImportance(element.importance);
+        props.setTimeframe(element.timeframe);
+        props.setDescription(element.description);
+        props.setObsolete(!element.obsolete);
+        setIsObsoleteId(element.id);
+    };
+
+
+    const tasks = props.tasks.map((element) => {
+        const milestone = format(element.milestone, 'dd-MM-yyyy');
+        const importance = element.obsolete ? (
+            <Icon path={mdiNull} size={1} color="lightgray" />
+        ) : element.importance < 1 ? (
+            ''
+        ) : element.importance < 2 ? (
+            <Icon path={mdiNumeric1Circle} size={1} color="red" />
+        ) : element.importance < 3 ? (
+            <Icon path={mdiNumeric2CircleOutline} size={1} color="orange" />
+        ) : (
+            <Icon path={mdiNumeric3} size={1} color="green" />
+        );
+
+        const project = props.projects.find((p) => p.id === element.projectId);
         return {
             col1: (
                 <a onClick={() => handleOnEdit(element)}>
-                    <Icon 
-                        path={mdiPencil}
-                        size={0.7}
-                    />
+                    <Icon path={mdiPencil} size={0.7} />
                 </a>
             ),
             col2: (
-                <a onClick={() => onDelete(element.id)}>
-                    <Icon 
-                        path={mdiDelete}
-                        size={0.7}
-                    />
+                <a onClick={() => onObsolete(element)}>
+                    <Icon path={element.obsolete ? mdiDeleteOff : mdiDelete} size={0.7} />
                 </a>
             ),
             col3: project.projectName,
@@ -52,6 +110,7 @@ function TaskTable(props: TaskTableProps) {
             col6: element.timeframe,
             col7: milestone,
             col8: importance,
+            obsolete: element.obsolete,
         };
     });
     const data = React.useMemo(() => tasks, [props.tasks]);
@@ -94,44 +153,70 @@ function TaskTable(props: TaskTableProps) {
         []
     );
 
-    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-        useTable({ columns, data });
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
 
     return (
-        <table
-            {...getTableProps({
-                className:
-                    'table is-bordered is-narrow is-hoverable is-fullwidth',
-            })}
-        >
-            <thead>
-                {headerGroups.map((headerGroup, i) => (
-                    <tr {...headerGroup.getHeaderGroupProps()} key={i}>
-                        {headerGroup.headers.map((column, i) => (
-                            <th {...column.getHeaderProps()} key={i}>
-                                {column.render('Header')}
-                            </th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {rows.map((row, i) => {
-                    prepareRow(row);
-                    return (
-                        <tr {...row.getRowProps()} key={i}>
-                            {row.cells.map((cell, i) => {
-                                return (
-                                    <td {...cell.getCellProps()} key={i}>
-                                        {cell.render('Cell')}
-                                    </td>
-                                );
-                            })}
-                        </tr>
-                    );
+        <>
+            <table
+                {...getTableProps({
+                    className: 'table is-bordered is-narrow is-hoverable is-fullwidth',
                 })}
-            </tbody>
-        </table>
+            >
+                <thead>
+                    {headerGroups.map((headerGroup, i) => (
+                        <tr {...headerGroup.getHeaderGroupProps()} key={i}>
+                            {headerGroup.headers.map((column, i) => (
+                                <th {...column.getHeaderProps()} key={i}>
+                                    {column.render('Header')}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row, i) => {
+                        // console.log('row: ', row.original.obsolete);
+                        prepareRow(row);
+                        return (
+                            <tr
+                                {...row.getRowProps({
+                                    className: row.original.obsolete ? 'has-background-grey has-text-info-light is-italic' : '',
+                                })}
+                                key={i}
+                            >
+                                {row.cells.map((cell, i) => {
+                                    return (
+                                        <td {...cell.getCellProps()} key={i}>
+                                            {cell.render('Cell')}
+                                        </td>
+                                    );
+                                })}
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+            <div className={'modal' + (isObsoleteId ? ' is-active' : '')}>
+                <div className="modal-background" onClick={closeModal}></div>
+                <div className="modal-content">
+                    <div className="box">
+                        {isObsoleteId && (
+                            <p>
+                                Vil du slette de registrede tid? <br />
+                                {/* {getTaskDescription(isObsoleteId)} */}
+                            </p>
+                        )}
+                        <button className="button is-danger" onClick={confirmObsolete}>
+                            Slet
+                        </button>
+                        <button className="button" onClick={closeModal}>
+                            Annuler
+                        </button>
+                    </div>
+                </div>
+                <button className="modal-close is-large" aria-label="close" onClick={closeModal}></button>
+            </div>
+        </>
     );
 }
 export default TaskTable;
